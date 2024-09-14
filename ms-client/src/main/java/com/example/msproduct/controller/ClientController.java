5e461949b348e13542960a914a0500f9dd1d8bb9
abdul8377 <abdulalfonso77@gmail.com>
2024-09-14 17:44:45 -0500

    ms-client

--- ms-client/src/main/java/com/example/msproduct/controller/ClientController.java
+++ ms-client/src/main/java/com/example/msproduct/controller/ClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Product")
+@RequestMapping("/Client")
 public class ClientController {
     @Autowired
     private ClientService clientService;
