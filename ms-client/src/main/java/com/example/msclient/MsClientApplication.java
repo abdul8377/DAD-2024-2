5753a92275d949ba3b7cce563b1f448ab54339eb
abdul8377 <tu-email@ejemplo.com>
2024-09-30 10:38:14 -0500

    jwt security

--- ms-client/src/main/java/com/example/msclient/MsClientApplication.java
+++ ms-client/src/main/java/com/example/msclient/MsClientApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MsClientApplication {
+public class  MsClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsClientApplication.class, args);
